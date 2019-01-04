db7ae035b4dd7f1bd1d9f2c41ebb7c1c8a7a92b2
Geetika2710 <geetikatakkar27@gmail.com>
2019-01-04 11:05:11 +0530

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+// function randomValueFromArray(array){
+//     var random = Math.floor(Math.random()*array.length);
+//     return array[random];
+//   }
+
 function Darken()
   {
-    var dark = document.getElementsByClassName("dark");
+    var D= document.getElementsByClassName("dark");
     
+    
+    var d='Darken';
+    var d1='lighten';
+    var d2= d.replace('d1');
+    var d2= d1.replace('d');
+    D.innerHtml=d2;
+
+
+
   }
